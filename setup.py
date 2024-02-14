be8544828cc5a2974eaa4f007271e60ecb0582df
Lucas Depetris <lucasdepetris14@gmail.com>
2024-02-14 18:24:35 -0300

    Arreglo final para fix de log filename

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='electracommons',
-    version='0.5.1.4',
+    version='0.5.1.5',
     author='Lucas Depetris',
     author_email='lucasdepetris14@gmail.com',
     description='Módulos comunes de los proyectos de Electra',
-    packages=['electracommons'],
+    # packages=['electracommons'],
+    packages=find_packages(),
     install_requires=[],
 )
